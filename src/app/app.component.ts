8faa89a4b757573808b5dd06e62ba38c37aa3f35
Abdulaziz <iuser1304@gmail.com>
2021-02-03 22:33:24 +0500

    validation logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ConfirmedValidator} from '../services/confirmed.validator';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'validator';
+  form: FormGroup = new FormGroup({});
+
+  constructor(private fb: FormBuilder) {
+
+    this.form = fb.group({
+      password: ['', [Validators.required]],
+      confirm_password: ['', [Validators.required]]
+    }, {
+      validator: ConfirmedValidator('password', 'confirm_password')
+    });
+  }
+
+  get controls(): any {
+    return this.form.controls;
+  }
+
+  submit(): any {
+    console.log(this.form.value);
+  }
+
 }
